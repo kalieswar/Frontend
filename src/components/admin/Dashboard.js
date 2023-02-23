45a8099816e0bb128fed58bd483f6b9b61341ee4
kalieswar <kaliesbhuvanesh25@gmail.com>
2023-02-23 22:04:29 +0530

    edits

--- src/components/admin/Dashboard.js
+++ src/components/admin/Dashboard.js
@@ -38,7 +38,7 @@ export default function Dashboard() {
             </div>
             <div className="col-12 col-md-10">
                 <h1 className="my-4">Dashboard</h1>
-                <div className="row pr-4">
+                <div className="row pe-4">
                     <div className="col-xl-12 col-sm-12 mb-3">
                         <div className="card text-white bg-primary o-hidden h-100">
                             <div className="card-body">
@@ -49,7 +49,7 @@ export default function Dashboard() {
                     </div>
                 </div>
 
-                <div className="row pr-4">
+                <div className="row pe-4">
                     <div className="col-xl-3 col-sm-6 mb-3">
                         <div className="card text-white bg-success o-hidden h-100">
                             <div className="card-body">
--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
-        <footer className="py-1 mt-3">
+        <footer className="py-5 mt-3">
             <p className="text-center text-dark mt-3 ">
-                Mycart - 2022-2023, All Rights Reserved
+                MaxGrocery - 2022-2023, All Rights Reserved
             </p>
         </footer>
     )
